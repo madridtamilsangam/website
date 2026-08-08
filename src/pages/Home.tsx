cd386ea197baedc24d9ee694ab4cb635de7fae55
Madrid Tamil Sangam <madridtamilasociacion@gmail.com>
2026-08-08 07:07:45 +0200

    home changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -46,6 +46,22 @@ export default function Home() {
           </article>
         ))}
       </div>
+
+      <div className="form-container">
+        <h2>{t('home.registration')}</h2>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfuQ9I8w5GJLxDbtDBNCcjv-vSD6_k3kLfpZHrgacx2wEASSQ/viewform?embedded=true"
+          width="100%"
+          height="600"
+          frameBorder="0"
+          marginHeight={0}
+          marginWidth={0}
+          className="embedded-form"
+          title="Registration Form"
+        >
+          Loading…
+        </iframe>
+      </div>
     </section>
   )
 }
